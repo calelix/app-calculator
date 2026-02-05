9a2af1aea76ee8eb6b0d6c2be142f9885e65cd75
Jonggwang Park <jgpark7281@gmail.com>
2026-02-06 00:12:23 +0900

    feat: SafeAreaView 및 StatusBar, NavigationBar 적용

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,21 @@
-import { Stack } from "expo-router";
+import { useEffect } from "react"
+import { Platform } from "react-native"
+
+import * as NavigationBar from "expo-navigation-bar"
+import { Stack } from "expo-router"
+import { StatusBar } from "expo-status-bar"
 
 export default function RootLayout() {
-  return <Stack />;
+  useEffect(() => {
+    if (Platform.OS === "android") {
+      NavigationBar.setStyle("light")
+    }
+  }, [])
+  
+  return (
+    <>
+      <StatusBar style="dark" />
+      <Stack />
+    </>
+  )
 }
