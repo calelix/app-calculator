292c3fb07e24c8ba3d0cc011d8c43bcfe6dcfb05
Jonggwang Park <jgpark7281@gmail.com>
2026-02-06 14:56:55 +0900

    feat: 예시 이미지 적용

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,20 @@
-import { Text } from "react-native"
+import {
+  Text
+} from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
+
+import { Image } from "expo-image"
 
 export default function Index() {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <Text style={{ fontFamily: 'PretendardBold' }}>
+    <SafeAreaView style={{ display: "flex", flexDirection: "column", gap: 10 }}>
+      <Text style={{ fontFamily: "PretendardBold" }}>
         Title
       </Text>
+      <Image
+        source={require("@/assets/images/example.png")}
+        style={{ width: 200, height: 200 }}
+      />
     </SafeAreaView>
   )
 }
